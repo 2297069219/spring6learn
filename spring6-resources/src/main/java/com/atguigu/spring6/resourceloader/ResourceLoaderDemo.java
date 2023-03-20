e054af219a896e39dafebcb667ee9d2cf5739343
guan-programmer <2297069219@qq.com>
2023-03-20 19:45:03 +0800

    Initial commit

---
+++ spring6-resources/src/main/java/com/atguigu/spring6/resourceloader/ResourceLoaderDemo.java
@@ -0,0 +1,24 @@
+package com.atguigu.spring6.resourceloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.Resource;
+
+public class ResourceLoaderDemo {
+
+    @Test
+    public void demo1() {
+        ApplicationContext context = new ClassPathXmlApplicationContext();
+        Resource resource = context.getResource("atguigu.txt");
+        System.out.println(resource.getFilename());
+    }
+
+    @Test
+    public void demo2() {
+        ApplicationContext context = new FileSystemXmlApplicationContext();
+        Resource resource = context.getResource("atguigu.txt");
+        System.out.println(resource.getFilename());
+    }
+}
